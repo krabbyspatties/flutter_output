d2b55ff87e82547f39817fd64f8fa7a77276f798
test <keanneberonio70@gmail.com>
2025-05-16 10:35:39 +0800

    fasdfa

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mydesign/pages/home.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -16,28 +17,69 @@ class ProfilePage extends StatelessWidget {
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/placeholder.jpg'),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "April John Ballinas",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 5),
-            Text(
-              "ballinas@gmail.com",
-              style: TextStyle(fontSize: 18, color: Colors.grey),
-            ),
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/placeholder.jpg'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "April John Ballinas",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              Text(
+                "ballinas@gmail.com",
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+              ),
+              SizedBox(height: 35),
+              Text(
+                "I'm an enthusiastic IT student with a passion for technology and problem-solving. Currently, I'm honing my skills in areas like software development, networking, cybersecurity, and data management. ",
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+              )
+            ],
+          ),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.grey,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+
+          if (index == 0) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          } else if (index == 1) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => ProfilePage()),
+            );
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.store), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: "Orders",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+      ),
     );
+    
   }
+  
 }
